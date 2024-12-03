2715be489a4db0de594789ef509d6710e95b9ad6
Kazuho1222 <destiny5914@gmail.com>
2024-12-03 20:55:34 +0900

    first commit

--- graphql-server/src/server.ts
+++ graphql-server/src/server.ts
@@ -1,15 +1,26 @@
 import { ApolloServer } from "apollo-server";
 import { gql } from "apollo-server";
 
+const todos = [
+  { id: 1, title: "GraphQLを勉強する", completed: false },
+  { id: 2, title: "Reactを勉強する", completed: false },
+];
+
 const typeDefs = gql`
+  type Todo {
+    id: ID!
+    title: String!
+    completed: Boolean!
+  }
+
   type Query {
-    message: String
+    getTodos: [Todo!]!
   }
 `;
 
 const resolvers = {
   Query: {
-    message: () => "Hello World",
+    getTodos: () => todos,
   },
 };
 
